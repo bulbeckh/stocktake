a1da15528f8758365372ca6dd8696221fe19737b
Henry Bulbeck <henrybulbeck@gmail.com>
2026-05-27 14:57:32 +1000

    moved navigation section of launchfile to OpaqueFunction so we can have our yaml merge (costmaps and nav2_params) dependent on a LaunchConfiguration (robot_type)

--- stocktake_core/launch/launch3.py
+++ stocktake_core/launch/launch3.py
@@ -79,6 +79,138 @@ def validate_robot_type(context):
 
     return []
 
+def navigation_opaque_function(context, stocktake_core_dir, bringup_dir, *args, **kwargs):
+    '''We need to wrap this in an opaque function so that we have access to the 'robot_type'
+    launch configuration. Otherwise, we are unable to use the yaml merge functionality'''
+
+    robot_type = LaunchConfiguration('robot_type').perform(context)
+
+    ## Merge the nav2 main and costmaps file and then overwrite some values
+    if robot_type=='lidar':
+        costmap_path = os.path.join(stocktake_core_dir, 'config', 'lidar_costmaps.yaml')
+    elif robot_type=='rgbd':
+        costmap_path = os.path.join(stocktake_core_dir, 'config', 'rgbd_costmaps.yaml')
+    elif robot_type=='stereo':
+        ## TODO Add new stereo costmap configuration (might be same as rgbd)
+        costmap_path = os.path.join(stocktake_core_dir, 'config', 'rgbd_costmaps.yaml')
+    elif robot_type=='mono':
+        ## TODO Add new mono costmap configuration (might be same as rgbd)
+        costmap_path = os.path.join(stocktake_core_dir, 'config', 'rgbd_costmaps.yaml')
+
+    merged_params_file = merge_param_files(
+            os.path.join(stocktake_core_dir, 'config', 'nav2_params.yaml'),
+            costmap_path
+    )
+
+    ## Launch Rviz2 with Nav2 Rviz2 configuration
+    rviz_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(bringup_dir, 'launch', 'rviz_launch.py')),
+        launch_arguments={
+            'namespace': LaunchConfiguration('namespace'),
+            'use_sim_time': LaunchConfiguration('use_sim_time'),
+            'rviz_config': os.path.join(stocktake_core_dir, 'config', 'nav2_default_view.rviz'),
+        }.items(),
+    )
+
+    ## Remap the tf topics to relative namespaces so we can add namespace prefixes
+    tf_remappings = [('/tf', 'tf'), ('/tf_static', 'tf_static')]
+
+    navigation_lidar_slam_cmd = GroupAction(
+        [
+            PushROSNamespace(condition=IfCondition(LaunchConfiguration('use_namespace')), namespace=LaunchConfiguration('namespace')),
+            Node(
+                condition=IfCondition(LaunchConfiguration('use_composition')),
+                name='nav2_container',
+                package='rclcpp_components',
+                executable='component_container_isolated',
+                parameters=[merged_params_file, {'autostart': 'True'}],
+                arguments=['--ros-args', '--log-level', 'info'],
+                remappings=tf_remappings,
+                output='screen',
+            ),
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    os.path.join(bringup_dir, 'launch', 'slam_launch.py')
+                ),
+                ## With the 2D lidar sensor, we use slam_toolbox (launched inside container)
+                condition=IfCondition(PythonExpression(["'", LaunchConfiguration('robot_type'), "' == 'lidar' "])),
+                launch_arguments={
+                    'namespace': LaunchConfiguration('namespace'),
+                    'use_sim_time': LaunchConfiguration('use_sim_time'),
+                    'autostart': 'True',
+                    'use_respawn': LaunchConfiguration('use_respawn'),
+                    'params_file': LaunchConfiguration('slam_params_file'),
+                    #'params_file': slam_params_configured,
+                }.items(),
+            ),
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    # NOTE We use our own modified navigation_launch.py in which we remove the collision_monitor.
+                    # This is only for the non-lidar sensors and is due to a current issue where we get ~0.8Hz rate
+                    # for the pointcloud gz to ros bridge. Once fixed, we can bring back in. Also note, since we
+                    # removed the collision monitor, we bridge cmd_vel on /cmd_vel_smoothed (output of velocity_smoother)
+                    # rather than /cmd_vel (output of collision monitor)
+                    #
+                    # When we are not using the rgbd, we can launch the nav2_bringup navigation_launch
+                    #os.path.join(bringup_dir, 'launch', 'navigation_launch.py')
+                    os.path.join(stocktake_core_dir, 'launch', 'navigation_launch.py')
+                ),
+                condition=UnlessCondition(PythonExpression(["'", LaunchConfiguration('robot_type'), "' == 'lidar' "])),
+                launch_arguments={
+                    'namespace': LaunchConfiguration('namespace'),
+                    'use_sim_time': LaunchConfiguration('use_sim_time'),
+                    'autostart': 'True',
+                    'params_file': merged_params_file,
+                    'use_composition': LaunchConfiguration('use_composition'),
+                    'use_respawn': LaunchConfiguration('use_respawn'),
+                    'container_name': 'nav2_container',
+                }.items(),
+            ),
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    os.path.join(bringup_dir, 'launch', 'navigation_launch.py')
+                ),
+                condition=IfCondition(PythonExpression(["'", LaunchConfiguration('robot_type'), "' == 'lidar' "])),
+                launch_arguments={
+                    'namespace': LaunchConfiguration('namespace'),
+                    'use_sim_time': LaunchConfiguration('use_sim_time'),
+                    'autostart': 'True',
+                    'params_file': merged_params_file,
+                    'use_composition': LaunchConfiguration('use_composition'),
+                    'use_respawn': LaunchConfiguration('use_respawn'),
+                    'container_name': 'nav2_container',
+                }.items(),
+            ),
+        ]
+    )
+
+    ## Start map server if we are not using slam_toolbox (originally launched alongside slam_toolbox in slam_launch.py)
+    start_map_server = GroupAction(
+        condition=IfCondition(PythonExpression(["'", LaunchConfiguration('robot_type'), "' != 'lidar' "])),
+        actions=[
+            SetParameter('use_sim_time', 'True'),
+            Node(
+                package='nav2_map_server',
+                executable='map_saver_server',
+                output='screen',
+                respawn=LaunchConfiguration('use_respawn'),
+                respawn_delay=2.0,
+                arguments=['--ros-args', '--log-level', 'info'],
+                parameters=[merged_params_file],
+            ),
+            Node(
+                package='nav2_lifecycle_manager',
+                executable='lifecycle_manager',
+                name='lifecycle_manager_slam',
+                output='screen',
+                arguments=['--ros-args', '--log-level', 'info'],
+                parameters=[{'autostart': True}, {'node_names': ['map_saver']}],
+            ),
+        ]
+    )
+
+    return [start_map_server, navigation_lidar_slam_cmd, rviz_cmd]
+
 def generate_launch_description() -> LaunchDescription:
     # Get package directories
     bringup_dir        = get_package_share_directory('nav2_bringup')
@@ -182,116 +314,6 @@ def generate_launch_description() -> LaunchDescription:
     )
     """
 
-    ## Merge the nav2 main and costmaps file and then overwrite some values
-    ## TODO Remove the overwrite here - just do it in the file since it is now in config/
-    merged_params_file = merge_param_files(
-            os.path.join(stocktake_core_dir, 'config', 'nav2_params.yaml'),
-            os.path.join(stocktake_core_dir, 'config', 'rgbd_costmaps.yaml'),
-    )
-
-    configured_params = RewrittenYaml(
-        source_file=merged_params_file,
-        root_key=namespace,
-        param_rewrites={
-            'global_costmap.global_costmap.ros__parameters.origin_x': '-25.0',
-            'global_costmap.global_costmap.ros__parameters.origin_y': '-25.0',
-            'global_costmap.global_costmap.ros__parameters.obstacle_layer.scan.sensor_frame': 'store_layout/robotmodel/robot_lidar/robot_lidar',
-            'local_costmap.local_costmap.ros__parameters.voxel_layer.scan.sensor_frame': 'store_layout/robotmodel/robot_lidar/robot_lidar',
-            'collision_monitor.ros__parameters.scan.topic': '/scan',
-            'local_costmap.local_costmap.ros__parameters.inflation_layer.inflation_radius': '0.4',
-            'global_costmap.global_costmap.ros__parameters.inflation_layer.inflation_radius': '0.4',
-        },
-        convert_types=True,
-    )
-
-    ## Launch Rviz2 with Nav2 Rviz2 configuration
-    rviz_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(bringup_dir, 'launch', 'rviz_launch.py')),
-        launch_arguments={
-            'namespace': namespace,
-            'use_sim_time': use_sim_time,
-            'rviz_config': os.path.join(stocktake_core_dir, 'config', 'nav2_default_view.rviz'),
-        }.items(),
-    )
-
-    ## Remap the tf topics to relative namespaces so we can add namespace prefixes
-    tf_remappings = [('/tf', 'tf'), ('/tf_static', 'tf_static')]
-
-    navigation_lidar_slam_cmd = GroupAction(
-        [
-            PushROSNamespace(condition=IfCondition(use_namespace), namespace=namespace),
-            Node(
-                condition=IfCondition(use_composition),
-                name='nav2_container',
-                package='rclcpp_components',
-                executable='component_container_isolated',
-                parameters=[merged_params_file, {'autostart': 'True'}],
-                arguments=['--ros-args', '--log-level', 'info'],
-                remappings=tf_remappings,
-                output='screen',
-            ),
-            IncludeLaunchDescription(
-                PythonLaunchDescriptionSource(
-                    os.path.join(bringup_dir, 'launch', 'slam_launch.py')
-                ),
-                ## With the 2D lidar sensor, we use slam_toolbox (launched inside container)
-                condition=IfCondition(PythonExpression(["'", LaunchConfiguration('robot_type'), "' == 'lidar' "])),
-                launch_arguments={
-                    'namespace': namespace,
-                    'use_sim_time': use_sim_time,
-                    'autostart': 'True',
-                    'use_respawn': use_respawn,
-                    'params_file': slam_params_file,
-                    #'params_file': slam_params_configured,
-                }.items(),
-            ),
-            IncludeLaunchDescription(
-                PythonLaunchDescriptionSource(
-                    # NOTE We use our own modified navigation_launch.py in which we remove the collision_monitor.
-                    # This is only for the non-lidar sensors and is due to a current issue where we get ~0.8Hz rate
-                    # for the pointcloud gz to ros bridge. Once fixed, we can bring back in. Also note, since we
-                    # removed the collision monitor, we bridge cmd_vel on /cmd_vel_smoothed (output of velocity_smoother)
-                    # rather than /cmd_vel (output of collision monitor)
-                    #os.path.join(bringup_dir, 'launch', 'navigation_launch.py')
-                    os.path.join(stocktake_core_dir, 'launch', 'navigation_launch.py')
-                ),
-                launch_arguments={
-                    'namespace': namespace,
-                    'use_sim_time': use_sim_time,
-                    'autostart': 'True',
-                    'params_file': merged_params_file,
-                    'use_composition': use_composition,
-                    'use_respawn': use_respawn,
-                    'container_name': 'nav2_container',
-                }.items(),
-            ),
-        ]
-    )
-
-    ## Start map server if we are not using slam_toolbox (originally launched alongside slam_toolbox in slam_launch.py)
-    start_map_server = GroupAction(
-        condition=IfCondition(PythonExpression(["'", LaunchConfiguration('robot_type'), "' != 'lidar' "])),
-        actions=[
-            SetParameter('use_sim_time', 'True'),
-            Node(
-                package='nav2_map_server',
-                executable='map_saver_server',
-                output='screen',
-                respawn=use_respawn,
-                respawn_delay=2.0,
-                arguments=['--ros-args', '--log-level', 'info'],
-                parameters=[merged_params_file],
-            ),
-            Node(
-                package='nav2_lifecycle_manager',
-                executable='lifecycle_manager',
-                name='lifecycle_manager_slam',
-                output='screen',
-                arguments=['--ros-args', '--log-level', 'info'],
-                parameters=[{'autostart': True}, {'node_names': ['map_saver']}],
-            ),
-        ]
-    )
 
     ## Transform tree
     # NOTE We could use robot_state_publisher for this but less control over transforms
@@ -426,6 +448,7 @@ def generate_launch_description() -> LaunchDescription:
 
     # Declare the launch options
     ld.add_action(declare_namespace_cmd)
+    ld.add_action(declare_use_namespace_cmd)
     ld.add_action(declare_use_sim_time_cmd)
     ld.add_action(declare_params_file_cmd)
     ld.add_action(declare_costmaps_file_cmd)
@@ -444,16 +467,18 @@ def generate_launch_description() -> LaunchDescription:
     ld.add_action(front_camera_optical_cmd)
 
 
-    ld.add_action(rviz_cmd)
 
     ld.add_action(octomap_node_cmd)
     ld.add_action(octomap_rviz_cmd)
 
-    ld.add_action(start_map_server)
+    #ld.add_action(start_map_server)
+    #ld.add_action(navigation_lidar_slam_cmd)
+    #ld.add_action(rviz_cmd)
+    ld.add_action(OpaqueFunction(function=navigation_opaque_function,
+                                 args=[stocktake_core_dir, bringup_dir]))
 
     ld.add_action(declare_robot_type_cmd)
     ld.add_action(robot_type_validation_cmd)
 
-    ld.add_action(navigation_lidar_slam_cmd)
 
     return ld
